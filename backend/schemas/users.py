1997130ee41ec3ee46804938c5685707f9b260df
Harshith <harshithravindra020@gmail.com>
2026-07-01 16:54:30 +0530

    test 11

---
+++ backend/schemas/users.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    role: str
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    role: str
+
+    class Config:
+        from_attributes = True
