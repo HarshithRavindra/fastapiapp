b2ebb9a808bbfa53a53cb30ea0a9f87298655360
Harshith <harshithravindra020@gmail.com>
2026-07-01 12:39:18 +0530

    test 10

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,50 +1,82 @@
-import Welcome from "./components/Welcome";
+// import Welcome from "./components/Welcome";
 import NavBar from "./components/NavBar";
 import CompanyCard from "./components/CompanyCard";
 import JobCard from "./components/JobCard";
 import Footer from "./components/Footer";
-import { useState, useEffect } from "react";
-import { getCompanies } from "./Services/CompanyService";
-import type {Company} from "./types/company";
+import {useEffect,useState} from "react";
+import { getCompanies,updateCompany,deleteCompany,createCompany } from "./Services/CompanyService";
+import type {Company} from "./types/company"
 
 function App(){
-  const [loading,setLoading]=useState(true);
+  const [loading,setLoading] = useState(true);
   const [error,setError] = useState<Error | null>(null)
   const [companies,setCompanies] = useState<Company[]>([]);
 
   async function fetchCompanies() {
     setLoading(true);
-    try{
+    try {
       const companies = await getCompanies();
       setCompanies(companies);
-
-    }catch (error){
+    } catch (error) {
       setError(error as Error);
-    } finally{
+    } finally {
       setLoading(false);
     }
   }
 
-  useEffect(()=>{
+  async function handleEdit(company:Company){
+    try{
+      const updatedCompany = await updateCompany(company.id,company);
+      setCompanies(companies.map((company) => company.id === updatedCompany.id ? updatedCompany : company));
+    }catch(error){
+      setError(error as Error);
+    }
+  }
+
+  async function handleDelete(id:number){
+    try{
+      await deleteCompany(id);
+      setCompanies(companies.filter((company) => company.id !== id));
+    }catch(error){
+      setError(error as Error);
+    }
+  }
+
+  async function handleAdd(company:Company){
+    try{
+      const newCompany = await createCompany(company);
+      setCompanies([...companies,newCompany]);
+    }catch(error){
+      setError(error as Error);
+    }
+  }
+
+
+  useEffect(() => {
     fetchCompanies();
   }, []);
-
+  
   if(loading){
-    return <div> Loading..</div>
+    return <div>Loading...</div>
   }
 
   if(error){
-    return <div>Error:{error.message}</div>
+    return <div>Error: {error.message}</div>
   }
-
+  
   return(
     <>
-    <NavBar/>
-    <Welcome/>
-    <br/>
-    <CompanyCard companies={companies}/>
-    <JobCard/>
-    <Footer/>
+    <NavBar />
+    {/* <Welcome /> */}
+    <br />
+    <CompanyCard 
+    companies={companies}
+    onedit={handleEdit}
+    ondelete={handleDelete}
+    onadd={handleAdd}
+    />
+    <JobCard />
+    <Footer />
     </>
   )
 }
